661394218d0b5f4c0545867080c8ec131f5874eb
Dilnaza <dilnazaturdumatova@gmail.com>
2024-03-26 23:40:33 +0600

    homework 26/03 наследование и инкапсуляция

---
+++ src/Homework/Lesson32_incapsulation_practice/PhoneTest.java
@@ -0,0 +1,16 @@
+package Homework.Lesson32_incapsulation_practice;
+
+public class PhoneTest {
+    public static void main(String[] args) {
+
+        double [] beelain = new double[]{1,1.2,30};
+        double [] o = new double[]{0.95,2,30};
+        double [] megakom = new double[]{1.05,1.7,40};
+
+        Phone phone = new Phone(beelain,o,megakom);
+
+        phone.makeCall();
+        phone.makeSms();
+        phone.useInternet();
+    }
+}
